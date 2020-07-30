5a2c4423adc70696c4f2d7bb81bd57d2d09a9210
Jerome Miranda <jerome.miranda@volenday.com>
2020-07-30 13:37:09 +0800

    v3.0.0

--- index.js
+++ index.js
@@ -1,20 +1,18 @@
 import React from 'react';
 import InputIcon from '@volenday/input-icon';
 
-export default ({ editable = false, headerStyle = {}, id, onChange, style = {}, ...defaultProps }) => {
+export default ({ editable = false, id, onChange, ...defaultProps }) => {
 	return {
 		...defaultProps,
-		filterable: false,
-		style: { ...style, display: 'flex', alignItems: 'center' },
-		headerStyle: { ...headerStyle, display: 'flex', alignItems: 'center' },
-		Cell: ({ original, value }) => {
-			if (typeof value == 'undefined') return null;
+		disableFilters: true,
+		Cell: ({ row, value }) => {
+			if (typeof value === 'undefined') return null;
 
 			if (editable) {
 				return (
 					<InputIcon
 						id={id}
-						onChange={(e, field, value) => onChange({ Id: original.Id, [field]: value })}
+						onChange={(e, field, value) => onChange({ Id: row.Id, [field]: value })}
 						withLabel={false}
 						value={value}
 						placeholder="Select an icon"
