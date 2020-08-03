0efe62c8edbc7d80e904f1271005e685570bea98
Jerome Miranda <jerome.miranda@volenday.com>
2020-08-03 14:52:54 +0800

    v3.0.1

--- index.js
+++ index.js
@@ -5,14 +5,14 @@ export default ({ editable = false, id, onChange, ...defaultProps }) => {
 	return {
 		...defaultProps,
 		disableFilters: true,
-		Cell: ({ row, value }) => {
+		Cell: ({ row: { original }, value }) => {
 			if (typeof value === 'undefined') return null;
 
 			if (editable) {
 				return (
 					<InputIcon
 						id={id}
-						onChange={(e, field, value) => onChange({ Id: row.Id, [field]: value })}
+						onChange={(e, field, value) => onChange({ Id: original.Id, [field]: value })}
 						withLabel={false}
 						value={value}
 						placeholder="Select an icon"
