ccc40b3e59174ff9743291a24dcbb9a6887f7b18
reynan <reynan.estolora@volenday.com>
2020-12-01 13:44:34 +0800

    Update: input process.browser then update packages.

--- index.js
+++ index.js
@@ -2,15 +2,18 @@ import React, { memo, Suspense } from 'react';
 import { Skeleton } from 'antd';
 import InputIcon from '@volenday/input-icon';
 
+const browser = typeof process.browser !== 'undefined' ? process.browser : true;
+
 export default ({ editable = false, id, onChange, ...defaultProps }) => {
 	return {
 		...defaultProps,
 		disableFilters: true,
-		Cell: props => (
-			<Suspense fallback={<Skeleton active={true} paragraph={null} />}>
-				<Cell {...props} other={{ editable, id, onChange }} />
-			</Suspense>
-		)
+		Cell: props =>
+			browser ? (
+				<Suspense fallback={<Skeleton active={true} paragraph={null} />}>
+					<Cell {...props} other={{ editable, id, onChange }} />
+				</Suspense>
+			) : null
 	};
 };
 
@@ -18,7 +21,8 @@ const Cell = memo(({ other: { editable, id, onChange }, row: { original }, value
 	if (typeof value === 'undefined' || !value) return null;
 
 	if (editable) {
-		return (
+		const InputIcon = require('@volenday/input-icon').default;
+		return browser ? (
 			<InputIcon
 				id={id}
 				onChange={(e, field, value) => onChange({ Id: original.Id, [field]: value })}
@@ -26,7 +30,7 @@ const Cell = memo(({ other: { editable, id, onChange }, row: { original }, value
 				withLabel={false}
 				value={value}
 			/>
-		);
+		) : null;
 	}
 
 	return <i className={value} />;
